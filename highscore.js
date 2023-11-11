418b2eef6b5c78ef1b838888439569b3c618e20e
camillebagnani <140850865+camillebagnani@users.noreply.github.com>
2023-11-10 18:07:48 -0700

    added custom readme file

--- highscore.js
+++ highscore.js
@@ -1,6 +1,9 @@
+// returns the string of 'scores' as an object, or starts a new array if there are no saved scores
 var highScores = JSON.parse(localStorage.getItem("scores")) || []
+// as long as the index is less than the length of the highscores, add a new score withe the initals and time, at the current index
 for(var i=0; i<highScores.length; i++) {
     var score = document.createElement("p");
     score.innerText = highScores[i].initials + " " + highScores[i].time;
+    // add the score to the HTML
     document.getElementById("scores").appendChild(score)
 }
